9d26957b93e95b56c1a6bfef0b49a3f0c141ae9a
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-07-21 13:59:08 +0530

    styled todo-input component

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,8 +2,8 @@ import "./TodoInput.css";
 export default function TodoInput() {
   return (
     <div className="todo-input-container">
-      <input type="text" placeholder="New Todo" />
-      <button>Add new task</button>
+      <input type="text" placeholder="New Todo" className="todo-input" />
+      <button className="add-todo-btn">Add new task</button>
     </div>
   );
 }
